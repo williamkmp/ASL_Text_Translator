7cf3d0ec7231f37aa1309bb173ed16084c547c24
William Kurnia Mulyadi Putra <81504006+williamkmp@users.noreply.github.com>
2021-06-05 16:28:59 +0800

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -50,7 +50,6 @@ function delClick(){
     alphabetString = alphabetString.slice(0, -1);
     updateTextArea();
     updateAslArea();
-    console.log(alphabetString);
 }
 
 document.getElementById('txtArea').addEventListener('input', function (event) {
@@ -72,8 +71,13 @@ function swapClick(){
         for (i = 0; i < aslKeys.length; i++) {
             aslKeys[i].style.display = "flex";
         }
-        txtArea.style.transform = "translateX(100%)";
-        aslArea.style.transform = "translateX(-100%)";
+
+        txtArea.classList.add('swapTextGo');
+        aslArea.classList.add('swapAslGo');
+        
+        if(txtArea.classList.contains('swapTextBack')) txtArea.classList.remove('swapTextBack');
+        if(aslArea.classList.contains('swapAslBack')) aslArea.classList.remove('swapAslBack');
+        
     }else{
         for (i = 0; i < aslKeys.length; i++) {
             aslKeys[i].style.display = "none";
@@ -81,8 +85,11 @@ function swapClick(){
         for (i = 0; i < letterKeys.length; i++) {
             letterKeys[i].style.display = "flex";
         }
-        txtArea.style.transform = "translateX(0%)";
-        aslArea.style.transform = "translateX(0%)";
+        txtArea.classList.add('swapTextBack');
+        aslArea.classList.add('swapAslBack');
+        
+        if(txtArea.classList.contains('swapTextGo')) txtArea.classList.remove('swapTextGo');
+        if(aslArea.classList.contains('swapAslGo')) aslArea.classList.remove('swapAslGo');
     }
 
     isAlphabet =  !isAlphabet;
@@ -92,9 +99,25 @@ function press(char){
     alphabetString = alphabetString + char + "";
     updateTextArea();
     updateAslArea();
-    console.log(char)
 }
 
 function settingButton(){
+    let menuContainer = document.getElementById('menuContainer');
+    console.log('setting');
 
+    if(menuContainer.classList.contains('menuExit')){
+        menuContainer.classList.remove('menuExit');
+        menuContainer.classList.add('menuShow');
+        setTimeout
+    }else if(menuContainer.classList.contains('menuShow')){
+        menuContainer.classList.remove('menuShow');
+        menuContainer.classList.add('menuExit');
+    }
+}
+
+function updateFontLable(){
+    let slider = document.getElementById('slider');
+    let label = document.getElementById('sizeLabel');
+    document.documentElement.style.setProperty('--font-size', slider.value.toString() + 'px');
+    label.innerHTML = "Font Size : " + slider.value.toString() + " px";
 }
